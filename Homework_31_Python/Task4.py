2601322aa889df33a19bc4327c24d209d3c16887
Vladislav Khvorov <vb0om@yandex.by>
2022-09-14 11:26:12 +0300

    Changed Task4.py

--- Homework_31_Python/Task4.py
+++ Homework_31_Python/Task4.py
@@ -1,16 +1,20 @@
 ### Task4 ###
 # Define decorator, that store function's calls
+memo = {}
 def cache(func):
-    memo = {}
-    def wrapper(*args):
-        if not memo.get(args):
-            g = func(*args)
-            memo[args] = g
+    def wrapper(*args, **kwargs):
+        if len(args):
+            dict_key = (func.__name__ + str(args))
+        else:
+            dict_key = (func.__name__ + str(kwargs))
+        if not memo.get(dict_key):
+            g = func(*args, **kwargs)
+            memo[dict_key] = g
             print('Function is caching now')
             return g
         else:
             print('This value from cache')
-            return memo[args]
+            return memo[dict_key]
     return wrapper
 
 @cache
@@ -19,14 +23,21 @@ def multiply(x,y):
     m = x*y
     return m
 
-# l1 = multiply(18,4)
-# print(l1)
-
 @cache
 # Define func, that devide args
 def devide(x,y):
     d = x/y
     return d
 
-# l2 = devide(18,3)
-# print(l2)
+l1 = multiply(x=18,y=4)
+print(l1)
+print(memo)
+l2 = devide(18,3)
+print(l2)
+print(memo)
+l2 = devide(18,2)
+print(l2)
+print(memo)
+l2 = devide(18,3)
+print(l2)
+print(memo)
